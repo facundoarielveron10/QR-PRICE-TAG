3561d94019b2515231934ba929c884c803bcfb27
Facundo Veron <facu_ve_2016@hotmail.com>
2023-10-03 17:49:36 -0300

    Comenzando el proyecto

---
+++ src/main/java/unnoba/poo/QRPriceTag/QrPriceTagApplication.java
@@ -0,0 +1,13 @@
+package unnoba.poo.QRPriceTag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrPriceTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrPriceTagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unnoba/poo/QRPriceTag/QrPriceTagApplicationTests.java
@@ -0,0 +1,13 @@
+package unnoba.poo.QRPriceTag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrPriceTagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
