0b5f1b29948db915f251ef3b1bb89ee32264e22e
Facundo Veron <facu_ve_2016@hotmail.com>
2023-10-19 18:29:41 -0300

    Agregando la denegacion a la pagina / sin autenticacion

--- src/main/java/unnoba/poo/QRPriceTag/config/SecurityConfig.java
+++ src/main/java/unnoba/poo/QRPriceTag/config/SecurityConfig.java
@@ -1,15 +1,10 @@
 package unnoba.poo.QRPriceTag.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,7 +16,7 @@ public class SecurityConfig implements WebMvcConfigurer {
         http
                 .authorizeHttpRequests((request) -> request
                         .requestMatchers("/webjars/**", "/resources/**","/css/**", "/img/**", "/js/**", "/register", "/login").permitAll()
-                        .requestMatchers("/**").authenticated()
+                        .requestMatchers("index.html").authenticated()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login").permitAll().defaultSuccessUrl("/").failureUrl("/login?error=true")
